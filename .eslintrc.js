9da0a40a39f84f73bef1b9061ec9cd50db83c123
Junior Paula <slucivaldo509@gmail.com>
2021-12-14 20:02:29 -0300

    configurate eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
