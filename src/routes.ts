396a3e91432edb1a91ef2dfe13204f7e8537f568
Junior Paula <slucivaldo509@gmail.com>
2021-12-19 11:18:32 -0300

    created method to delete one category by id

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { DeleteCategoryController } from './controllers/DeleteCategoryController';
 import { GetAllCategoryController } from './controllers/GetAllCategoryController';
 
 const routes = Router();
 
 routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoryController().handle);
+routes.delete('/categories/:id', new DeleteCategoryController().handle);
 
 export { routes };
