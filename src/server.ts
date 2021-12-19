b4fbe07752214b9225f48f996b107c408259cd10
Junior Paula <slucivaldo509@gmail.com>
2021-12-19 10:23:41 -0300

    created conection database

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata';
 
 import express from 'express';
 
+/** importar a conexão */
+import './database';
+
 const app = express();
 
 app.listen(3005, () => console.log('server is running...'));
