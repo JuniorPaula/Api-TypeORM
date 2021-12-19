149f35f51cc1d074c01292474c05c551158b47b4
Junior Paula <slucivaldo509@gmail.com>
2021-12-19 11:52:53 -0300

    method to create videos

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { CreateVideoController } from './controllers/CreateVideoController';
 import { DeleteCategoryController } from './controllers/DeleteCategoryController';
 import { GetAllCategoryController } from './controllers/GetAllCategoryController';
 import { UpdateCategoryController } from './controllers/UpdateCategoryController';
@@ -10,5 +11,7 @@ routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoryController().handle);
 routes.delete('/categories/:id', new DeleteCategoryController().handle);
 routes.put('/categories/:id', new UpdateCategoryController().handle);
+
+routes.post('/videos', new CreateVideoController().handle);
 
 export { routes };
