445fa54c7b4d28488f0cb546f5460555c18d49ae
Junior Paula <slucivaldo509@gmail.com>
2021-12-19 11:08:37 -0300

    created method to get all categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { GetAllCategoryController } from './controllers/GetAllCategoryController';
 
 const routes = Router();
 
 routes.post('/categories', new CreateCategoryController().handle);
+routes.get('/categories', new GetAllCategoryController().handle);
 
 export { routes };
