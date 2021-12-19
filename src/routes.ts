324bf77253d73979e964849a9b009e245ad1a7bb
Junior Paula <slucivaldo509@gmail.com>
2021-12-19 11:37:46 -0300

    created method to update one category

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
 import { DeleteCategoryController } from './controllers/DeleteCategoryController';
 import { GetAllCategoryController } from './controllers/GetAllCategoryController';
+import { UpdateCategoryController } from './controllers/UpdateCategoryController';
 
 const routes = Router();
 
 routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoryController().handle);
 routes.delete('/categories/:id', new DeleteCategoryController().handle);
+routes.put('/categories/:id', new UpdateCategoryController().handle);
 
 export { routes };
